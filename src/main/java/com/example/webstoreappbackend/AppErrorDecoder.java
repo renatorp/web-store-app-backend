2d7eb954daaf8a6cbdf57404261fa33188caa290
Renato Ribeiro <r3nato.rp+github@gmail.com>
2018-10-07 00:53:50 -0300

    Including bad request feign exception handling

--- src/main/java/com/example/webstoreappbackend/AppErrorDecoder.java
+++ src/main/java/com/example/webstoreappbackend/AppErrorDecoder.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.example.webstoreappbackend.exception.BadRequestException;
 import com.example.webstoreappbackend.exception.NotFoundException;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
@@ -17,19 +18,27 @@ public class AppErrorDecoder implements ErrorDecoder {
 	private final ErrorDecoder defaultErrorDecoder = new Default();
 
 	private static final Logger log = LoggerFactory.getLogger(AppErrorDecoder.class);
-	
+
 	@Override
 	public Exception decode(String methodKey, Response response) {
-		if (response.status() == 404) {
+		if (response.status() == 404 || response.status() == 400) {
 			try {
 				JsonParser jsonParser = new JsonParser();
 				JsonElement parse = jsonParser.parse(response.body().asReader());
-				throw new NotFoundException(parse.getAsJsonObject().get("message").getAsString());
+				String message = parse.getAsJsonObject().get("message").getAsString();
+
+				switch (response.status()) {
+				case 404:
+					throw new NotFoundException(message);
+				case 400:
+					throw new BadRequestException(message);
+				}
+
 			} catch (IOException e) {
 				log.error("ERROR", e);
 			}
 		}
-		
+
 		return defaultErrorDecoder.decode(methodKey, response);
 	}
 }
